214a4f0d29bc674716d3ca70233c19483857a361
Betty-Ojeda <bettyojeda.azteca@gmail.com>
2023-01-14 23:42:56 -0600

    se usa libreria numpy

--- Calculadora4.py
+++ Calculadora4.py
@@ -1,7 +1,47 @@
+#Libreria
 import math
+import numpy as np
 
+#
+def suma():
+    num1 = float(input("Ingresa el primer número: "))
+    num2 = float(input("Ingresa el segundo número: "))
+    print(num1, "+", num2, "=", num1 + num2)
 
+def resta():
+    num1 = float(input("Ingresa el primer número: "))
+    num2 = float(input("Ingresa el segundo número: "))
+    print(num1, "-", num2, "=", num1 - num2)
+
+def division():
+    num1 = float(input("Ingresa el primer número: "))
+    num2 = float(input("Ingresa el segundo número: "))
+    print(num1, "/", num2, "=", num1 / num2)
+
+def multiplicacion():
+    num1 = float(input("Ingresa el primer número: "))
+    num2 = float(input("Ingresa el segundo número: "))
+    print(num1, "*", num2, "=", num1 * num2)
+
+def raiz():
+    num1 = float(input("Ingresa el númera para sacar su raiz: "))
+    raiz=math.sqrt(num1)
+    print("La raiz cuadrada de ",  num1,  " es ",  raiz)
+
+def exponente():
+    base = float(input("Ingresa la base: "))
+    exponente = float(input("Ingresa el exponente: "))
+    resultado=math.pow(base, exponente)
+    print("El resultado de ",  base,  "elevado al ",  exponente, "es ", resultado )
+
+def seno():
+    num1 = float(input("Ingresa el número: "))
+    resultado=np.sin(np.deg2rad(num1))
+    print("El seno de " , num1, "es ", resultado )
+
+#
 def calculadora():
+    #
     while True:
         print("Selecciona la operación")
         print("1. Suma")
@@ -13,37 +53,24 @@ def calculadora():
         print("7. Seno")
 
 
-        choice = input("Ingresa tu elección (1/2/3/4/5/6/7): ")
 
-        if choice == '1':
-            num1 = float(input("Ingresa el primer número: "))
-            num2 = float(input("Ingresa el segundo número: "))
-            print(num1, "+", num2, "=", num1 + num2)
-        elif choice == '2':
-            num1 = float(input("Ingresa el primer número: "))
-            num2 = float(input("Ingresa el segundo número: "))
-            print(num1, "-", num2, "=", num1 - num2)
-        elif choice == '3':
-            num1 = float(input("Ingresa el primer número: "))
-            num2 = float(input("Ingresa el segundo número: "))
-            print(num1, "*", num2, "=", num1 * num2)
-        elif choice == '4':
-            num1 = float(input("Ingresa el primer número: "))
-            num2 = float(input("Ingresa el segundo número: "))
-            print(num1, "*", num2, "=", num1 * num2)
-        elif choice == '5':
-            num1 = float(input("Ingresa el númera para sacar su raiz: "))
-            raiz=math.sqrt(num1)
-            print("La raiz cuadrada de ",  num1,  " es ",  raiz)
-        elif choice == '6':
-            base = float(input("Ingresa la base: "))
-            exponente = float(input("Ingresa el exponente: "))
-            resultado=math.pow(base, exponente)
-            print("El resultado de ",  base,  "elevado al ",  exponente, "es ", resultado )
-        elif choice == '7':
-            num1 = float(input("Ingresa el número: "))
-            resultado=math.sin(math.radians(num1))
-            print("El seno de " , num1, "es ", resultado )
+
+        operacion = input("Ingresa tu elección (1/2/3/4/5/6/7): ")
+
+        if operacion == '1':
+            suma()    
+        elif operacion == '2':
+            resta()
+        elif operacion == '3':
+            multiplicacion()
+        elif operacion == '4':
+            division()
+        elif operacion == '5':
+            raiz()
+        elif operacion == '6':
+            exponente()
+        elif operacion == '7':
+            seno()
                  
         else:
             print("Opción inválida")
